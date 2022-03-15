f5011729ddbdeedd6fa7fa62b95fd543fa08e586
tivimateplaylists <72917634+tivimateplaylists@users.noreply.github.com>
2022-03-16 02:07:39 +0530

    Add files via upload

---
+++ JioTV-main/token.php
@@ -0,0 +1,41 @@
+<?php
+
+# © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
+
+# Here I Put Token which Available Publicly I Recommended Use Your Own Token Here 
+# For Suppport @Avishkarpatil at Telegram or proavipatil@gmail.com
+
+
+
+$jctBase = "cutibeau2ic"; # Channge To Your Token
+
+$ssoToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWUiOiIzYjg5MzU5YS0xMDM5LTQxYmYtOGU2NS0xNDNkOTdkOWNjNDIiLCJ1c2VyVHlwZSI6IlJJTHBlcnNvbiIsImF1dGhMZXZlbCI6IjQwIiwiZGV2aWNlSWQiOiI4ODAyNjZhY2I1NDcyYmJjYWYwZTY0ZWE2YmE0NWE2ZDM1ZTAxNzVkZWQ2NTk4OTIwNGM0NDRhNzlhN2U3MWM4MDA0YWVkNTJlMDgyYWYzN2JkYjk1NGJiNTYxYjM3YmMxOWViOWMzNWMwZmI2MWFjOWVlZjE2ZjM3OTMwOWExNiIsImp0aSI6ImY3NmY5MjFhLTU5Y2YtNGQ3Ni1iZDY5LThmODVhY2NiZDE2MCIsImlhdCI6MTY0NzM3NTczOX0.YfYW_aGNzyYhb0HVMLaO1iLqAdwX7Y3N5bXTIH7Hf8I"; #Change to Your Token
+function tokformat($str)
+{
+$str= base64_encode(md5($str,true));
+return str_replace("\n","",str_replace("\r","",str_replace("/","_",str_replace("+","-",str_replace("=","",$str)))));
+}
+function generateJct($st, $pxe) 
+{
+ global $jctBase;
+ return trim(tokformat($jctBase . $st . $pxe));
+}
+function generatePxe() {
+return time() + 6000000;
+}
+function generateSt() {
+global $ssoToken;
+return tokformat($ssoToken);
+}
+function generateToken() {
+$st = generateSt();
+$pxe = generatePxe();
+$jct = generateJct($st, $pxe);
+return "?jct=" . $jct . "&pxe=" . $pxe . "&st=" . $st;
+}
+
+# © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
+
+
+echo generateToken();
+?>
